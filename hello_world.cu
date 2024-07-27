87fae6a283574ad26e4a5993bb0639977092fc0e
yashcgosavi <yash@yash>
2024-07-27 20:26:31 +0530

    host device intro

--- hello_world.cu
+++ hello_world.cu
@@ -1,7 +1,17 @@
 #include "common/book.h"
+__global__ void add(int a, int b, int* c)
+{
+    *c = a + b;
+}
 
 int main(void)
 {
-    printf("Hello world!\n");
+    int c;
+    int* dev_c;
+    cudaMalloc((void**) &dev_c, sizeof(int));
+    add<<<1, 1>>>(2, 3, dev_c);
+    cudaMemcpy(&c, &dev_c, sizeof(int), cudaMemcpyHostToDevice);
+    printf("2+3=%d\n", c);
+    cudaFree(dev_c);
     return(0);
 }
